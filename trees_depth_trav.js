e9775c40d6414d43d98a78b90447a3941bfc0547
Erika Szabo <erika.szabo.geo@gmail.com>
2018-12-17 23:55:34 +0000

    calculates total subordinates including boss

--- trees_depth_trav.js
+++ trees_depth_trav.js
@@ -34,6 +34,15 @@ class Employee {
 
     // Use depth first traversal to calculate the total employees
 
+    if (this) {
+      totalEmployees = 1;
+    }
+
+    for (const subordinate of this.subordinates) {
+      const totalEmployees1 = subordinate.totalEmployees;
+      totalEmployees = totalEmployees + 1;
+   }
+
     return totalEmployees;
 
   }
@@ -44,10 +53,15 @@ const craig    = new Employee("Craig", "VP Software", 1000000);
 const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
 const angela   = new Employee("Angela", "VP Retail", 1000000);
 const phil     = new Employee("Phil", "VP Marketing", 1000000);
+const ali = new Employee('Ali');
+const simone = new Employee('Simone');
 ada.addSubordinate(craig);
 ada.addSubordinate(arvinder);
 ada.addSubordinate(angela);
 ada.addSubordinate(phil);
+craig.addSubordinate(ali);
+craig.addSubordinate(simone);
 
-console.log(ada.employeesThatMakeOver(418401));
+// console.log(ada.employeesThatMakeOver(418401));
+console.log(craig.totalEmployees);
 
