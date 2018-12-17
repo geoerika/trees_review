077fe78e3a8e368ac33cf83625d7dba43390d02b
Erika Szabo <erika.szabo.geo@gmail.com>
2018-12-17 23:39:44 +0000

    setting up file for tree traversal functions

---
+++ trees_depth_trav.js
@@ -0,0 +1,43 @@
+class Employee {
+  constructor(name, title, salary) {
+    this.name = name;
+    this.title = title;
+    this.salary = salary;
+    this.boss = null;
+    this.subordinates = [];
+  }
+
+  addSubordinate(subordinate) {
+    this.subordinates.push(subordinate);
+    subordinate.boss = this;
+  }
+
+  employeesThatMakeOver(amount) {
+
+    let employees = []; // 1
+
+    if (this.salary > amount) {
+      employees.push(this); // 2
+    }
+
+    for (const subordinate of this.subordinates) {
+      const subordinatesThatMakeOver = subordinate.employeesThatMakeOver(amount); // 3
+      employees = employees.concat(subordinatesThatMakeOver);
+    }
+
+    return employees;
+  }
+}
+
+const ada = new Employee("Ada", "CEO", 3000000.00);
+const craig    = new Employee("Craig", "VP Software", 1000000);
+const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
+const angela   = new Employee("Angela", "VP Retail", 1000000);
+const phil     = new Employee("Phil", "VP Marketing", 1000000);
+ada.addSubordinate(craig);
+ada.addSubordinate(arvinder);
+ada.addSubordinate(angela);
+ada.addSubordinate(phil);
+
+console.log(ada.employeesThatMakeOver(418401));
+
